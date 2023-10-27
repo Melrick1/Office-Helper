a2c97c1cf896e7d66efd5c4f4f0d7c8b99e0dd4f
Melrick1 <melrickbatuwael@gmail.com>
2023-10-28 01:38:43 +0800

    Finished Schedule Features

--- Student Helper/Form1.Designer.cs
+++ Student Helper/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -35,8 +36,19 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.dataSet1 = new System.Data.DataSet();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.Day = new System.Windows.Forms.ComboBox();
+            this.ScheduleInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -105,18 +117,122 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(168, 84);
+            this.dataGridView1.Location = new System.Drawing.Point(168, 119);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(620, 305);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(168, 26);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(69, 31);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "New Row";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(338, 26);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(69, 31);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Select :";
+            // 
+            // Day
+            // 
+            this.Day.FormattingEnabled = true;
+            this.Day.Items.AddRange(new object[] {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"});
+            this.Day.Location = new System.Drawing.Point(168, 83);
+            this.Day.Margin = new System.Windows.Forms.Padding(2);
+            this.Day.Name = "Day";
+            this.Day.Size = new System.Drawing.Size(101, 21);
+            this.Day.TabIndex = 10;
+            this.Day.Text = "Day";
+            this.Day.SelectedIndexChanged += new System.EventHandler(this.Day_SelectedIndexChanged);
+            // 
+            // ScheduleInput
+            // 
+            this.ScheduleInput.Location = new System.Drawing.Point(290, 83);
+            this.ScheduleInput.Name = "ScheduleInput";
+            this.ScheduleInput.Size = new System.Drawing.Size(100, 20);
+            this.ScheduleInput.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Data :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(253, 26);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(69, 31);
+            this.buttonUpdate.TabIndex = 13;
+            this.buttonUpdate.Text = "Add";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(423, 26);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(69, 31);
+            this.buttonClear.TabIndex = 14;
+            this.buttonClear.Text = "Clear Row";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ScheduleInput);
+            this.Controls.Add(this.Day);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -124,7 +240,9 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -137,6 +255,16 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Day;
+        private System.Windows.Forms.TextBox ScheduleInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonClear;
     }
 }
 
--- Student Helper/Form1.cs
+++ Student Helper/Form1.cs
@@ -62,6 +62,118 @@ namespace Student_Helper
 
         }
 
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            //Insert (Only for new Rows)
+            if (koneksi.State != ConnectionState.Open)
+            {
+                koneksi.Open();
+            }
+            try
+            {
+                query = string.Format("insert into schedule_table ({0}) values ('{1}')", Day.Text, ScheduleInput.Text);
+                perintah = new MySqlCommand(query, koneksi);
+                adapter = new MySqlDataAdapter(perintah);
+                int res = perintah.ExecuteNonQuery();
+                koneksi.Close();
+                if (res == 1)
+                {
+                    MessageBox.Show("Insert data berhasil");
+                    Form1_Load(null, null);
+                }
+                else
+                {
+                    MessageBox.Show("Insert data gagal");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            //Update (If row exists)
+            if (koneksi.State != ConnectionState.Open)
+            {
+                koneksi.Open();
+            }
+            try
+            {
+                query = string.Format("update schedule_table set {0} = '{1}' where {0}=''", Day.Text, ScheduleInput.Text);
+                perintah = new MySqlCommand(query, koneksi);
+                adapter = new MySqlDataAdapter(perintah);
+                perintah.ExecuteNonQuery();
+                ds.Clear();
+                adapter.Fill(ds);
+                koneksi.Close();
+
+                Form1_Load(null, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (koneksi.State != ConnectionState.Open)
+            {
+                koneksi.Open();
+            }
+            try
+            {
+                query = string.Format("update schedule_table set {0} = '' where {0} = '{1}'", Day.Text, ScheduleInput.Text);
+                perintah = new MySqlCommand(query, koneksi);
+                adapter = new MySqlDataAdapter(perintah);
+                perintah.ExecuteNonQuery();
+                ds.Clear();
+                adapter.Fill(ds);
+                koneksi.Close();
+
+                Form1_Load(null, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            if (koneksi.State != ConnectionState.Open)
+            {
+                koneksi.Open();
+            }
+            try
+            {
+                query = string.Format("delete from schedule_table where {0} = '{1}'", Day.Text, ScheduleInput.Text);
+                perintah = new MySqlCommand(query, koneksi);
+                adapter = new MySqlDataAdapter(perintah);
+                perintah.ExecuteNonQuery();
+                ds.Clear();
+                adapter.Fill(ds);
+                koneksi.Close();
+
+                Form1_Load(null, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Day_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
             try
@@ -77,20 +189,20 @@ namespace Student_Helper
                 koneksi.Close();
 
                 dataGridView1.DataSource = ds.Tables[0];
-                dataGridView1.Columns[0].Width = 100;
+                dataGridView1.Columns[0].Width = 75;
                 dataGridView1.Columns[0].HeaderText = "Monday";
-                dataGridView1.Columns[1].Width = 100;
+                dataGridView1.Columns[1].Width = 75;
                 dataGridView1.Columns[1].HeaderText = "Tuesday";
-                dataGridView1.Columns[0].Width = 100;
-                dataGridView1.Columns[0].HeaderText = "Wednesday";
-                dataGridView1.Columns[0].Width = 100;
-                dataGridView1.Columns[0].HeaderText = "Thursday";
-                dataGridView1.Columns[0].Width = 100;
-                dataGridView1.Columns[0].HeaderText = "Friday";
-                dataGridView1.Columns[0].Width = 100;
-                dataGridView1.Columns[0].HeaderText = "Saturday";
-                dataGridView1.Columns[0].Width = 100;
-                dataGridView1.Columns[0].HeaderText = "Sunday";
+                dataGridView1.Columns[2].Width = 75;
+                dataGridView1.Columns[2].HeaderText = "Wednesday";
+                dataGridView1.Columns[3].Width = 75;
+                dataGridView1.Columns[3].HeaderText = "Thursday";
+                dataGridView1.Columns[4].Width = 75;
+                dataGridView1.Columns[4].HeaderText = "Friday";
+                dataGridView1.Columns[5].Width = 75;
+                dataGridView1.Columns[5].HeaderText = "Saturday";
+                dataGridView1.Columns[6].Width = 75;
+                dataGridView1.Columns[6].HeaderText = "Sunday";
             }
             catch (Exception ex)
             {
