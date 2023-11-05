e93acb226f0365bd0a9c0aef68f303ec193c9fcd
Melrick1 <melrickbatuwael@gmail.com>
2023-11-05 15:46:35 +0800

    minor changes

--- Student Helper/CalendarForm.Designer.cs
+++ Student Helper/CalendarForm.Designer.cs
@@ -58,18 +58,16 @@
             this.panel1.Controls.Add(this.buttonSchedule);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(265, 838);
+            this.panel1.Size = new System.Drawing.Size(199, 681);
             this.panel1.TabIndex = 5;
             // 
             // buttonCalendar
             // 
             this.buttonCalendar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonCalendar.Location = new System.Drawing.Point(48, 469);
-            this.buttonCalendar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonCalendar.Location = new System.Drawing.Point(36, 381);
             this.buttonCalendar.Name = "buttonCalendar";
-            this.buttonCalendar.Size = new System.Drawing.Size(167, 43);
+            this.buttonCalendar.Size = new System.Drawing.Size(125, 35);
             this.buttonCalendar.TabIndex = 3;
             this.buttonCalendar.Text = "Calendar";
             this.buttonCalendar.UseVisualStyleBackColor = true;
@@ -78,10 +76,9 @@
             // buttonNotes
             // 
             this.buttonNotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonNotes.Location = new System.Drawing.Point(48, 406);
-            this.buttonNotes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonNotes.Location = new System.Drawing.Point(36, 330);
             this.buttonNotes.Name = "buttonNotes";
-            this.buttonNotes.Size = new System.Drawing.Size(167, 43);
+            this.buttonNotes.Size = new System.Drawing.Size(125, 35);
             this.buttonNotes.TabIndex = 2;
             this.buttonNotes.Text = "Notes";
             this.buttonNotes.UseVisualStyleBackColor = true;
@@ -90,10 +87,9 @@
             // buttonToDo
             // 
             this.buttonToDo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonToDo.Location = new System.Drawing.Point(48, 343);
-            this.buttonToDo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonToDo.Location = new System.Drawing.Point(36, 279);
             this.buttonToDo.Name = "buttonToDo";
-            this.buttonToDo.Size = new System.Drawing.Size(167, 43);
+            this.buttonToDo.Size = new System.Drawing.Size(125, 35);
             this.buttonToDo.TabIndex = 2;
             this.buttonToDo.Text = "To-Do List";
             this.buttonToDo.UseVisualStyleBackColor = true;
@@ -102,10 +98,9 @@
             // buttonReminder
             // 
             this.buttonReminder.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonReminder.Location = new System.Drawing.Point(48, 281);
-            this.buttonReminder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonReminder.Location = new System.Drawing.Point(36, 228);
             this.buttonReminder.Name = "buttonReminder";
-            this.buttonReminder.Size = new System.Drawing.Size(167, 43);
+            this.buttonReminder.Size = new System.Drawing.Size(125, 35);
             this.buttonReminder.TabIndex = 1;
             this.buttonReminder.Text = "Reminder";
             this.buttonReminder.UseVisualStyleBackColor = true;
@@ -114,10 +109,9 @@
             // buttonSchedule
             // 
             this.buttonSchedule.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonSchedule.Location = new System.Drawing.Point(48, 218);
-            this.buttonSchedule.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonSchedule.Location = new System.Drawing.Point(36, 177);
             this.buttonSchedule.Name = "buttonSchedule";
-            this.buttonSchedule.Size = new System.Drawing.Size(167, 43);
+            this.buttonSchedule.Size = new System.Drawing.Size(125, 35);
             this.buttonSchedule.TabIndex = 0;
             this.buttonSchedule.Text = "Schedule";
             this.buttonSchedule.UseVisualStyleBackColor = true;
@@ -125,19 +119,16 @@
             // 
             // dayContainer
             // 
-            this.dayContainer.Location = new System.Drawing.Point(308, 138);
-            this.dayContainer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dayContainer.Location = new System.Drawing.Point(231, 112);
             this.dayContainer.Name = "dayContainer";
-            this.dayContainer.Size = new System.Drawing.Size(1352, 629);
+            this.dayContainer.Size = new System.Drawing.Size(1014, 511);
             this.dayContainer.TabIndex = 6;
-            this.dayContainer.Paint += new System.Windows.Forms.PaintEventHandler(this.dayContainer_Paint);
             // 
             // NextBtn
             // 
-            this.NextBtn.Location = new System.Drawing.Point(1544, 793);
-            this.NextBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.NextBtn.Location = new System.Drawing.Point(1158, 644);
             this.NextBtn.Name = "NextBtn";
-            this.NextBtn.Size = new System.Drawing.Size(109, 31);
+            this.NextBtn.Size = new System.Drawing.Size(82, 25);
             this.NextBtn.TabIndex = 7;
             this.NextBtn.Text = "Next";
             this.NextBtn.UseVisualStyleBackColor = true;
@@ -145,10 +136,9 @@
             // 
             // PrevBtn
             // 
-            this.PrevBtn.Location = new System.Drawing.Point(1411, 793);
-            this.PrevBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.PrevBtn.Location = new System.Drawing.Point(1058, 644);
             this.PrevBtn.Name = "PrevBtn";
-            this.PrevBtn.Size = new System.Drawing.Size(109, 31);
+            this.PrevBtn.Size = new System.Drawing.Size(82, 25);
             this.PrevBtn.TabIndex = 8;
             this.PrevBtn.Text = "Previous";
             this.PrevBtn.UseVisualStyleBackColor = true;
@@ -158,10 +148,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(361, 110);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(271, 89);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.Size = new System.Drawing.Size(63, 20);
             this.label1.TabIndex = 9;
             this.label1.Text = "Sunday";
             // 
@@ -169,10 +158,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(556, 110);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(417, 89);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 25);
+            this.label2.Size = new System.Drawing.Size(65, 20);
             this.label2.TabIndex = 10;
             this.label2.Text = "Monday";
             // 
@@ -180,10 +168,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(744, 111);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(558, 90);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(89, 25);
+            this.label3.Size = new System.Drawing.Size(69, 20);
             this.label3.TabIndex = 11;
             this.label3.Text = "Tuesday";
             // 
@@ -191,10 +178,9 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(916, 111);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(687, 90);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(118, 25);
+            this.label4.Size = new System.Drawing.Size(93, 20);
             this.label4.TabIndex = 12;
             this.label4.Text = "Wednesday";
             // 
@@ -202,10 +188,9 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(1127, 110);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(845, 89);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(95, 25);
+            this.label5.Size = new System.Drawing.Size(74, 20);
             this.label5.TabIndex = 13;
             this.label5.Text = "Thursday";
             // 
@@ -213,10 +198,9 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(1324, 110);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(993, 89);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(66, 25);
+            this.label6.Size = new System.Drawing.Size(52, 20);
             this.label6.TabIndex = 14;
             this.label6.Text = "Friday";
             // 
@@ -224,29 +208,27 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(1508, 110);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(1131, 89);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(91, 25);
+            this.label7.Size = new System.Drawing.Size(73, 20);
             this.label7.TabIndex = 15;
             this.label7.Text = "Saturday";
             // 
             // labelMonth
             // 
             this.labelMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelMonth.Location = new System.Drawing.Point(781, 52);
-            this.labelMonth.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelMonth.Location = new System.Drawing.Point(586, 42);
             this.labelMonth.Name = "labelMonth";
-            this.labelMonth.Size = new System.Drawing.Size(397, 37);
+            this.labelMonth.Size = new System.Drawing.Size(298, 30);
             this.labelMonth.TabIndex = 16;
             this.labelMonth.Text = "Month Year";
             this.labelMonth.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // CalendarForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1685, 838);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Controls.Add(this.labelMonth);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -259,7 +241,6 @@
             this.Controls.Add(this.NextBtn);
             this.Controls.Add(this.dayContainer);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "CalendarForm";
             this.Text = "Student-Helper | Calendar";
             this.Load += new System.EventHandler(this.CalendarForm_Load);
--- Student Helper/ToDoForm.Designer.cs
+++ Student Helper/ToDoForm.Designer.cs
@@ -28,12 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonCalendar = new System.Windows.Forms.Button();
+            this.buttonNotes = new System.Windows.Forms.Button();
+            this.buttonToDo = new System.Windows.Forms.Button();
+            this.buttonReminder = new System.Windows.Forms.Button();
+            this.buttonSchedule = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.panel1.Controls.Add(this.buttonCalendar);
+            this.panel1.Controls.Add(this.buttonNotes);
+            this.panel1.Controls.Add(this.buttonToDo);
+            this.panel1.Controls.Add(this.buttonReminder);
+            this.panel1.Controls.Add(this.buttonSchedule);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(199, 681);
+            this.panel1.TabIndex = 16;
+            // 
+            // buttonCalendar
+            // 
+            this.buttonCalendar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalendar.Location = new System.Drawing.Point(36, 381);
+            this.buttonCalendar.Name = "buttonCalendar";
+            this.buttonCalendar.Size = new System.Drawing.Size(125, 35);
+            this.buttonCalendar.TabIndex = 3;
+            this.buttonCalendar.Text = "Calendar";
+            this.buttonCalendar.UseVisualStyleBackColor = true;
+            // 
+            // buttonNotes
+            // 
+            this.buttonNotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNotes.Location = new System.Drawing.Point(36, 330);
+            this.buttonNotes.Name = "buttonNotes";
+            this.buttonNotes.Size = new System.Drawing.Size(125, 35);
+            this.buttonNotes.TabIndex = 2;
+            this.buttonNotes.Text = "Notes";
+            this.buttonNotes.UseVisualStyleBackColor = true;
+            // 
+            // buttonToDo
+            // 
+            this.buttonToDo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonToDo.Location = new System.Drawing.Point(36, 279);
+            this.buttonToDo.Name = "buttonToDo";
+            this.buttonToDo.Size = new System.Drawing.Size(125, 35);
+            this.buttonToDo.TabIndex = 2;
+            this.buttonToDo.Text = "To-Do List";
+            this.buttonToDo.UseVisualStyleBackColor = true;
+            // 
+            // buttonReminder
+            // 
+            this.buttonReminder.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReminder.Location = new System.Drawing.Point(36, 228);
+            this.buttonReminder.Name = "buttonReminder";
+            this.buttonReminder.Size = new System.Drawing.Size(125, 35);
+            this.buttonReminder.TabIndex = 1;
+            this.buttonReminder.Text = "Reminder";
+            this.buttonReminder.UseVisualStyleBackColor = true;
+            // 
+            // buttonSchedule
+            // 
+            this.buttonSchedule.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSchedule.Location = new System.Drawing.Point(36, 177);
+            this.buttonSchedule.Name = "buttonSchedule";
+            this.buttonSchedule.Size = new System.Drawing.Size(125, 35);
+            this.buttonSchedule.TabIndex = 0;
+            this.buttonSchedule.Text = "Schedule";
+            this.buttonSchedule.UseVisualStyleBackColor = true;
+            // 
+            // ToDoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.panel1);
+            this.Name = "ToDoForm";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonCalendar;
+        private System.Windows.Forms.Button buttonNotes;
+        private System.Windows.Forms.Button buttonToDo;
+        private System.Windows.Forms.Button buttonReminder;
+        private System.Windows.Forms.Button buttonSchedule;
     }
 }
